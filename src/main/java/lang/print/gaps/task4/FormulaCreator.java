2192bda8b654a1c7b44ef0d12a5f6f255224faa5
Nishanov Khasan <99598107+HasanNishanov@users.noreply.github.com>
2022-07-19 17:04:44 +0500

    task has beek finished

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        //(9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2
+        //(9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2
+        char a = 'a';
+        char b = 'b';
+
+        System.out.println((9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2);
+
+
+
     }
 }
